8bc15e85f8973c1879755fc7d82d219eed8127fc
Leon <chen-linming@163.com>
2013-11-28 16:41:47 +0800

    Initial commit

---
+++ out/production/dilu/main.go
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 13-11-28.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
---
+++ src/main.go
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 13-11-28.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+	var s int;
+	s = 7;
+	fmt.Printf("%d",s)
+}
